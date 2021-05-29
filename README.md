a60862da8f96f3004c53173b6cc69539ec49c8d7
Studio Fomalhaut <studio@fomalhaut.nl>
2021-05-29 21:30:05 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lawyerssite
+Lawyers Website
---
+++ precache-manifest.75933c8f1495de6f56da347547627403.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "b78e6baa0f90ae02b7ea",
+    "url": "/static/css/main.a4555f3d.chunk.css"
+  },
+  {
+    "revision": "b78e6baa0f90ae02b7ea",
+    "url": "/static/js/main.310b423e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "175eb27a06c4c57917be",
+    "url": "/static/js/2.11226ae6.chunk.js"
+  },
+  {
+    "revision": "853beb5e6c2c63ea4358db3cd3e16ed9",
+    "url": "/static/media/civil2.853beb5e.jpg"
+  },
+  {
+    "revision": "bc97757d81a7f5125ec28285369ed4ca",
+    "url": "/static/media/oam.bc97757d.png"
+  },
+  {
+    "revision": "14b40c61962eef2acdf43301db835709",
+    "url": "/static/media/logo-white.14b40c61.png"
+  },
+  {
+    "revision": "c1699c793e272553682b2130851f74e7",
+    "url": "/static/media/intro.c1699c79.jpg"
+  },
+  {
+    "revision": "1989590d437b785a7e4f07d6ea781b9e",
+    "url": "/static/media/bpartner.1989590d.jpg"
+  },
+  {
+    "revision": "8259a14dcd58fb30606ad2e5d630921a",
+    "url": "/static/media/civitas.8259a14d.png"
+  },
+  {
+    "revision": "369f3b7b17fd32a780ef6bc6de303dec",
+    "url": "/static/media/executive-protection.369f3b7b.png"
+  },
+  {
+    "revision": "309b230f528dd4eaeb277f8a99a58dc6",
+    "url": "/static/media/grupo_infra.309b230f.png"
+  },
+  {
+    "revision": "2c4b609e7b4d7f10b6746b2244b17245",
+    "url": "/static/media/intro1.2c4b609e.jpeg"
+  },
+  {
+    "revision": "40eed5d0b4cee875ce39d960362a235c",
+    "url": "/static/media/intro2.40eed5d0.jpeg"
+  },
+  {
+    "revision": "cc611d0d185fdb39c8cab5a8feb1c639",
+    "url": "/static/media/intro3.cc611d0d.jpg"
+  },
+  {
+    "revision": "2d7092a0c670089e841db68686b7d7fc",
+    "url": "/static/media/logo-gold.2d7092a0.png"
+  },
+  {
+    "revision": "37505cd907eab9030fd7acff9d10f702",
+    "url": "/static/media/quemsomos.37505cd9.jpeg"
+  },
+  {
+    "revision": "1d60449fd0cc41342104913efe0418ff",
+    "url": "/static/media/typing.1d60449f.jpg"
+  },
+  {
+    "revision": "a7c5dc2f765883c7ede4433ab737311d",
+    "url": "/static/media/familia.a7c5dc2f.jpg"
+  },
+  {
+    "revision": "46080adefd0b701856118954758287e8",
+    "url": "/static/media/imob.46080ade.jpg"
+  },
+  {
+    "revision": "bf5184026205b6baa61f3571781bde00",
+    "url": "/static/media/penal.bf518402.jpg"
+  },
+  {
+    "revision": "853beb5e6c2c63ea4358db3cd3e16ed9",
+    "url": "/static/media/civil.853beb5e.jpg"
+  },
+  {
+    "revision": "3979f452898c226cfeda9b3f8dad7b71",
+    "url": "/static/media/laboral.3979f452.jpg"
+  },
+  {
+    "revision": "b284fc7f1d543fb7cb8a425a54951176",
+    "url": "/static/media/comercial.b284fc7f.jpg"
+  },
+  {
+    "revision": "90048a1a2c5c4b5dc493fb734e8fc2fe",
+    "url": "/static/media/contencioso.90048a1a.jpg"
+  },
+  {
+    "revision": "605e4eb50e85a24d766f8ef3fcd2656d",
+    "url": "/static/media/migracao.605e4eb5.jpg"
+  },
+  {
+    "revision": "9ffc56582a3b5a5f01c5969f8d484275",
+    "url": "/static/media/sociedades.9ffc5658.jpg"
+  },
+  {
+    "revision": "dd8955fdd5cc4372bf9b80b369394463",
+    "url": "/static/media/internacional.dd8955fd.jpg"
+  },
+  {
+    "revision": "dd2d37eadd7775dbc596fc80b182d55f",
+    "url": "/static/media/Maputo-sm.dd2d37ea.jpg"
+  },
+  {
+    "revision": "b37e6afe5fb3d592e4fff580cdf00966",
+    "url": "/static/media/joburg-sm.b37e6afe.jpg"
+  },
+  {
+    "revision": "93ed8b5c750e0f4050e7218398124b23",
+    "url": "/static/media/portugal-sm.93ed8b5c.jpg"
+  },
+  {
+    "revision": "e7f2cd88b2a853685d341039f495502c",
+    "url": "/static/media/meeting.e7f2cd88.jpg"
+  },
+  {
+    "revision": "ae7b61c61d199fa50beec7006f5f2313",
+    "url": "/static/media/justice-crop.ae7b61c6.jpg"
+  },
+  {
+    "revision": "46080adefd0b701856118954758287e8",
+    "url": "/static/media/imobiliario.46080ade.jpg"
+  },
+  {
+    "revision": "b3bae48130df8c3093d428a2193a97b3",
+    "url": "/static/media/autoestrada.b3bae481.jpg"
+  },
+  {
+    "revision": "27648db566cf65b1a248771102229d9b",
+    "url": "/static/media/bullying.27648db5.jpg"
+  },
+  {
+    "revision": "4f1c06798ebc4a24ff867eaff551e177",
+    "url": "/static/media/Maputo.4f1c0679.jpg"
+  },
+  {
+    "revision": "72b9475341d9b5aaee6dc5ce56af402b",
+    "url": "/static/media/portugal.72b94753.jpeg"
+  },
+  {
+    "revision": "f86cf46f7ea43d0f482b7e97bc08ba0e",
+    "url": "/static/media/Johannesburg.f86cf46f.jpg"
+  },
+  {
+    "revision": "f0857f8b395fa5151210ec0f64f55634",
+    "url": "/index.html"
+  }
+];
